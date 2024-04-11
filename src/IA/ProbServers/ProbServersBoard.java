fc64f22f7f890dc5e9c5653f0e838d3a32b35a0b
Àngel <iamamc3@gmail.com>
2024-04-11 20:16:23 +0200

    Ara Peticions desa tmb l'usuari i s'inicialitza Temps

--- src/IA/ProbServers/ProbServersBoard.java
+++ src/IA/ProbServers/ProbServersBoard.java
@@ -10,15 +10,9 @@ public class ProbServersBoard {
 
     //* State data structure
 
-    private ArrayList<ArrayList<Integer>> FileServer;
-// private ArrayList<Pair<Integer,Pair<Integer, Integer>>> Req; eliminat perque no es pot declarar tipus dins el Pair
-
-    private ArrayList<ArrayList<Integer>> Peticions;              //vector de servidors on un int diu quin es el servidor que la facilita
-
-    private int [][] FSFiles;                                               //vector de FS de vectors amb els fitxers
-    //private ArrayList<Pair<Integer,Integer>> Files;                         //NO SE SI CAL. vector de Pair<Files, num> on num es el numero de vegades que esta el fitxer en un FS
-
-    private int [][] sol;           // vector solució dels FS amb la distribució dels fitxers
+    private ArrayList<ArrayList<Integer>> FileServer; // Els fitxers que conté cada servidor
+    private ArrayList<ArrayList<Integer[]>> Peticions; //vector de servidors on un int diu quin es el servidor que la facilita
+    private ArrayList<Integer> Temps; // Vector amb el temps total que triga un servidor a atendre les peticions que té assignades
 
     /* Constructor */
     public ProbServersBoard(Servers servers, Requests requests, int nserv) {
@@ -41,21 +35,36 @@ public class ProbServersBoard {
 
         Peticions = new ArrayList<>(nserv);
         for (int i = 0; i < nserv; ++i) {
-            ArrayList<Integer> ini = new ArrayList<>();
+            ArrayList<Integer[]> ini = new ArrayList<>();
             Peticions.add(i, ini);
         }
+
+        Temps = new ArrayList<>(nserv);
+        for (int i = 0; i < nserv; ++i) {
+            Temps.add(0);
+        }
+
         for (int req = 0; req < requests.size(); ++req){
             boolean exit = false;
             for (int i = 0; i < nserv && !exit; ++i) {
                 for (int j = 0; j < FileServer.get(i).size(); ++j) {
                     if (requests.getRequest(req)[1] == FileServer.get(i).get(j)) {
-                        Peticions.get(i).add(FileServer.get(i).get(j));
+                        Integer[] requestVec = {requests.getRequest(req)[0], requests.getRequest(req)[1]};
+                        // Assignar request a aquell servidor
+                        Peticions.get(i).add(requestVec);
+                        // Sumar el temps d'aquesta petició al total del servidor
+                        Temps.set(i, Temps.get(i)+servers.tranmissionTime(i, requests.getRequest(req)[0]));
                         System.out.println("peticio " + req + " de fitxer " + FileServer.get(i).get(j) + " al servidor " + i);
                         exit = true;
                         break;
                     }
                 }
             }
+        }
+
+        // Per imprimir el temps de cada servidor a l'estat inicial
+        for (int i = 0; i < nserv; ++i) {
+            System.out.println("Servidor " + i + ": temps " + Temps.get(i));
         }
 
 
